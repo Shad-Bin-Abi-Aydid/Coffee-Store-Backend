29e56057f5d144d7348a94ca37b7482936a5a512
shad-bin-abi-aydid <shadaydid@gmail.com>
2025-11-13 19:39:58 +0000

    Implement backend endpoints for user listing and deletion

--- index.js
+++ index.js
@@ -92,6 +92,20 @@ async function run() {
       res.send(result);
     })
 
+    // R operation or read the users from DB
+    app.get('/users', async(req, res) =>{
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    })
+
+    // D operation or Delete user from the database
+    app.delete('/users/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await userCollection.deleteOne(query)
+      res.send(result);
+    })
+
 
 
     // Send a ping to confirm a successful connection
